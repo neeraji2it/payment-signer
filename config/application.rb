81939e14dc33ce806547e47cb77e96cab4ffaa56
rails <rails@rails.com>
2015-04-07 15:55:45 +0530

    remove unwanted pages add required assets use sass over css use coffeescript over javascript configure gritter message

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,9 @@ Bundler.require(*Rails.groups)
 
 module PaymentSigner
   class Application < Rails::Application
+    config.generators.stylesheets = false
+    config.generators.javascripts = false
+    
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
