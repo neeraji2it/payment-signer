bf84c79f04edc27b6135d50ba0d4ff038031d8e9
rails <rails@rails.com>
2015-04-08 13:24:49 +0530

    cleanup validation codes generate gflash en file, which was missing

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,7 +1,8 @@
 class Payment < ActiveRecord::Base
-	validates :amount, :product_name, :customer_name, :address, :city, :state, :post_code, :country,:email, :card_number, :presence=>"true"
-	validates :phone, :presence => true, format: { with: /\A\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})\Z/,
-	message: " Phone numbers must be in xxx-xxx-xxxx format" }
-	validates :amount,  numericality: {only_float: true}
-	validates :post_code,:card_number, numericality: { only_integer: true }
+
+  # validation
+	validates :amount, :product_name, :customer_name, :address, :city, :state, 
+            :post_code, :country, :email, :card_number, :phone,
+            presence: true
+	validates :amount,  numericality: { only_float: true }
 end
