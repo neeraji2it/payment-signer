1dcf5825227b626eaa208f304874b092fabd3560
Ranjana Choudhary <rockingrails@gmail.com>
2015-07-15 19:47:33 +0530

    add ip address to setup

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,9 +1,9 @@
-role :app, %w{root@63.142.250.83}
-role :web, %w{root@63.142.250.83}
-role :db,  %w{root@63.142.250.83}
-server '63.142.250.83', user: 'root', roles: %w{web app db} #, my_property: :my_value
+role :app, %w{root@63.142.251.176}
+role :web, %w{root@63.142.251.176}
+role :db,  %w{root@63.142.251.176}
+server '63.142.251.176', user: 'root', roles: %w{web app db} #, my_property: :my_value
 
-set :domain, '63.142.250.83'
+set :domain, '63.142.251.176'
 set :deploy_to, '/var/www/payment-signer.com/public_html'
 set :branch, 'master'
 set :deploy_user, 'root'
