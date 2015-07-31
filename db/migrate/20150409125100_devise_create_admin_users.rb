3ef41eb54a4c4b9a76a3668e06a4f98b79caf1d5
rails <rails@rails.com>
2015-07-31 13:37:52 +0530

    first commit

--- db/migrate/20150409125100_devise_create_admin_users.rb
+++ db/migrate/20150409125100_devise_create_admin_users.rb
@@ -2,7 +2,7 @@ class DeviseCreateAdminUsers < ActiveRecord::Migration
   def migrate(direction)
     super
     # Create a default user
-    AdminUser.create!(email: ENV["ADMIN_EMAIL"], password: ENV["ADMIN_PASSWORD"], password_confirmation: ENV["ADMIN_PASSWORD"]) if direction == :up
+    AdminUser.create!(email: 'admin@rising-sun.com', password: 'dine-media12345', password_confirmation: 'dine-media12345') if direction == :up
   end
 
   def change
